83c6e6dec688134f1890146d4ba7448971fada80
Jonatas-Souza <jonatassouzapereira@gmail.com>
2023-10-22 14:39:48 -0300

    Adicionada as controllers Get Products/ e Get Products/id

---
+++ ApiCatalogo/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            try
+            {
+                var products = _context.Products.ToList();
+                if (products.IsNullOrEmpty())
+                {
+                    return NotFound("Produtos não encontrados...");
+                }
+
+                return products;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Product> Get(int id)
+        {
+            try
+            {
+                var product = _context.Products.FirstOrDefault(x => x.ProductId == id);
+                if (product is null)
+                {
+                    return NotFound("Produto não encontrado...");
+                }
+                return product;
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
