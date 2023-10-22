9acc7c1b759055a1bc4b812623b961407efe84e0
Jonatas-Souza <jonatassouzapereira@gmail.com>
2023-10-22 00:17:23 -0300

    Realizado Inserts iniciais nas tabelas Products e Categories por meio de duas novas Migrations

---
+++ ApiCatalogo/Migrations/20231022024250_InsertCategories.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories(Name,ImageUrl) " +
+                "                           Values('Bebidas','Bebidas.jpg')," +
+                "                               ('Lanches', 'Lanches.jpg')," +
+                "                               ('Sobremesas', 'Sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categories");
+        }
+    }
+}
---
+++ ApiCatalogo/Migrations/20231022025458_InsertProducts.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, ImageUrl, Stock, CreateDate,CategoryId) " +
+                "                           Values('Coca-Cola Diet','Refrigerante de Cola 350 ml',5.45,'CocaCola.jpg',50,getdate(),1)," +
+                "                                 ('Lanche de Atum','Lanche de Atum com maionese',8.50,'Atum.jpg',10,getdate(),2)," +
+                "                                 ('Pudim 100g','Pudim de leite condensado 100g',6.75,'Pudim.jpg',20,getdate(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Products");
+        }
+    }
+}
